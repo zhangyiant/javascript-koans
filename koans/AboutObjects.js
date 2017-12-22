2d8b8fe004612dd5ff3bf9a5e0dd708ff3a8ac3f
Peter-Yi Zhang <peter-yi.zhang@autodesk.com>
2017-12-22 17:27:56 +0800

    Fiexed shoudl confirm that when a function is attached to an object, 'this' refers to the object.

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -44,8 +44,8 @@ describe("About Objects", function () {
       }
     };
 
-    expect(currentYear).toBe(FILL_ME_IN);
-    expect(megalomaniac.calculateAge()).toBe(FILL_ME_IN);
+    expect(currentYear).toBe(2017);
+    expect(megalomaniac.calculateAge()).toBe(47);
   });
 
   describe("'in' keyword", function () {
