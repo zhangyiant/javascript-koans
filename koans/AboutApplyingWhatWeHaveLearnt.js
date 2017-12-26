e4b063dda3cc9f569af88a33ea4c0c1d0825bfb4
Peter-Yi Zhang <peter-yi.zhang@autodesk.com>
2017-12-26 11:21:55 +0800

    Fixed add all the natual numbers

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -62,14 +62,18 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum = _(_.range(1, 1000)).chain()
+        .filter(function(x) { return (x % 3 === 0 || x % 5 == 0) })
+        .reduce(function(memo, num) { return memo + num }, 0)
+        .value()
+    /* try chaining range() and reduce() */
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
